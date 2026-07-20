46c7e4602e76fb01cd07cd50ab3763ce28c5ea53
vahanbgs <51419074+vahanbgs@users.noreply.github.com>
2026-07-20 19:24:56 +0200

    feat: implement mod downloading

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use cart::{Instance, Launcher};
 use cli::{Cli, Subcommands};
 use config::Config;
 use manifest::{Manifest, ModDependency};
+use tokio::{fs, io};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -26,15 +27,29 @@ async fn main() -> anyhow::Result<()> {
             let launcher = Launcher::new();
             let cache = launcher.mod_cache();
 
-            for (_mod_name, mod_source) in &config.manifest().mods {
-                let _path = match mod_source {
+            let game_directory = config.manifest_directory().join("minecraft/");
+            let mods_directory = game_directory.join("mods/");
+            fs::create_dir_all(&mods_directory).await?;
+
+            for (mod_name, mod_source) in &config.manifest().mods {
+                let source_path = match mod_source {
                     ModDependency::Url { url } => cache.fetch_mod(&url).await?,
                 };
+
+                let target_path = mods_directory.join(format!("{mod_name}.jar"));
+
+                match fs::remove_file(&target_path).await {
+                    Ok(()) => {}
+                    Err(e) if e.kind() == io::ErrorKind::NotFound => {}
+                    Err(e) => Err(e)?,
+                }
+
+                fs::hard_link(source_path, target_path).await?;
             }
 
             let instance = Instance::builder()
                 .version(config.minecraft_version())
-                .build(config.manifest_directory().join("minecraft/"));
+                .build(game_directory);
 
             launcher.launch(&instance).await?;
         }
